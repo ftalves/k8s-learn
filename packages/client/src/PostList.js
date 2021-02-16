e159fa1c8b02e7700d15dbffb4856a3672369fb6
ftalves <ftelles.bus@gmail.com>
2021-02-16 17:04:10 -0300

    Add proper routes

--- packages/client/src/PostList.js
+++ packages/client/src/PostList.js
@@ -7,7 +7,7 @@ export default () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get('http://localhost:4002/posts');
+    const res = await axios.get('http://posts.com/posts');
 
     setPosts(res.data);
   };
@@ -25,7 +25,7 @@ export default () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList comments={post.comments} />
+          <CommentList comments={post.comments || []} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
